4bfb000d813499029c24dd3c9ced48bbd37e9195
edeediong <edidiongmichael08@gmail.com>
2020-05-31 22:12:42 +0100

    feat: implemented error handling

--- shortner/models.py
+++ shortner/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from hashlib import md5
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
+from graphql import GraphQLError
 
 # Create your models here.
 class URL(models.Model):
@@ -17,5 +21,11 @@ class URL(models.Model):
     def save(self, *args, **kwargs):
         if not self.id:
             self.url_hash = md5(self.full_url.encode()).hexdigest()[:10]
+
+        validate = URLValidator()
+        try:
+            validate(self.full_url)
+        except ValidationError as e:
+            raise GraphQLError('invalid url')
 
         return super().save(*args, **kwargs)
