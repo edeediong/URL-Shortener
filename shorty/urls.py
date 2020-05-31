c688bdbb8f145450ad5df82bd6c543f247b54748
edeediong <edidiongmichael08@gmail.com>
2020-05-31 21:43:37 +0100

    feat: creating the access endpoints

--- shorty/urls.py
+++ shorty/urls.py
@@ -19,7 +19,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
 
+from shortner.views import root
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('<str:url_hash>/', root, name='root'),
 ]
