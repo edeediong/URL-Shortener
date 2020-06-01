90d41db68a01db9208c17bc2498b46554afde2bb
edeediong <edidiongmichael08@gmail.com>
2020-06-01 02:35:22 +0100

    feat: implemented pagination

--- shortner/schema.py
+++ shortner/schema.py
@@ -9,14 +9,20 @@ class URLType(DjangoObjectType):
         model = URL
 
 class Query(graphene.ObjectType):
-    urls = graphene.List(URLType, url=graphene.String())
+    urls = graphene.List(URLType, url=graphene.String(), first=graphene.Int(), skip=graphene.Int())
 
-    def resolve_urls(self, info, url=None, **kwargs):
+    def resolve_urls(self, info, url=None, first=None, skip=None, **kwargs):
         queryset = URL.objects.all()
 
         if url:
             _filter = Q(full_url__icontains=url)
             queryset = queryset.filter(_filter)
+
+        if first:
+            queryset = queryset[:first]
+
+        if skip:
+            queryset = queryset[skip:]
 
         return queryset
 
